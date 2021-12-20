0e0eeb54a831343ce2a50657c828f2e40da9d776
Abhishek Batra <a1062000batra@gmail.com>
2021-12-20 15:24:02 +0530

    Added Search Functionality

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,13 +1,23 @@
-import React, { useContext } from 'react'
-import Recipe from './Recipe'
-import { RecipeContext } from './App'
+import React, { useContext, useState } from 'react';
+import Recipe from './Recipe';
+import { RecipeContext } from './App';
 
 export default function RecipeList({ recipes }) {
     const { handleRecipeAdd }=useContext(RecipeContext);
+    const [searchText, setSearchText] = useState();
+
+    function handleRecipeSearch(usrInput){
+        setSearchText(usrInput);
+    }
+    const filteredRecipes=searchText != null ? recipes.filter(r => r.name.toLowerCase().includes(searchText.toLowerCase())) : recipes;
     return (
         <div className='recipe-list'>
+            <div className="search-box__container">
+                <label className='search-box__label' htmlFor="search">Search a recipe</label>
+                <input className='search-box__input'  type="text" name="search" placeholder="Search for a recipe..." onChange={(e) => handleRecipeSearch(e.target.value)}/>
+            </div>
             <div>
-                {recipes.map((recipe)=>{
+                {filteredRecipes.map((recipe)=>{
                     return <Recipe key={recipe.id} {...recipe}/>
                 })}
             </div>
